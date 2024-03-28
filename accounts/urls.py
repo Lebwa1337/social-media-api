99c3ed25ed47449584d33768c8140524bd1d7a59
LeBwa <lebwa1337@gmail.com>
2024-03-28 20:53:50 +0200

    Implement registration/login/profile functionality for user

---
+++ accounts/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from accounts import views
+
+urlpatterns = [
+    path("register/", views.CreateUserView.as_view(), name="register"),
+    path("login/", views.ObtainTokenView.as_view(), name="login"),
+    path("me/", views.ManageUserView.as_view(), name="me")
+]
+
+
+app_name = 'accounts'
